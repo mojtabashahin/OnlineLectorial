39b1128649923f07fa5d5f44855e2c50b4d99e87
E116324 <E116324@localhost>
2022-05-19 15:12:27 +1000

    This is my first commit.

---
+++ OnlineLectorial/src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	private int ID;
+	private String fullname;
+	public Student (int studentID, String studentFullname)
+	{
+		ID = studentID;
+		fullname= studentFullname;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
